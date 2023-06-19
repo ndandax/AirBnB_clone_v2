458c6108495e152fb76724d4075b1bdb3bd28ab6
ndandax <sebandandala99@gmail.com>
2023-06-19 21:38:44 +0300

    prepare_db

---
+++ setup_mysql_dev.sql
@@ -0,0 +1,8 @@
+-- a script that prepares a MySQL server
+-- a database hbnb_dev_db
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
+FLUSH PRIVILEGES;
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
+FLUSH PRIVILEGES;
