1243236f62938794a81943dd5b7a9b01c45a0194
Hauwa Ibrahim <hauwaibrahim71@gmail.com>
2023-06-20 02:44:14 -0700

    AirBnB_clone_v2

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,8 +1,13 @@
 -- a script that prepares a MySQL server
 -- a database hbnb_dev_db
-CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
-CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
-GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
-FLUSH PRIVILEGES;
-GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+CREATE USER 
+    IF NOT EXISTS 'hbnb_test'@'localhost'
+    IDENTIFIED BY 'hbnb_test_pwd';
+GRANT ALL PRIVILEGES ON 'hbnb_test_db'.* 
+    TO 'hbnb_test'@'localhost';
+    IDENTIFIED BY 'hbnb_test_pwd';
+GRANT SELECT ON 'performance_schema'.*
+    TO 'hbnb_test'@'localhost';
+    IDENTIFIED BY 'hbnb_test_pwd';
 FLUSH PRIVILEGES;
