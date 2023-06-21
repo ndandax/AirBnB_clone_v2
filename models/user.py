365192563fdf6c6043f3ce84087dfa80ff4c76dc
ndandax <sebandandala99@gmail.com>
2023-06-21 16:19:45 +0300

    update

--- models/user.py
+++ models/user.py
@@ -3,45 +3,23 @@
 import hashlib
 import models
 from models.base_model import BaseModel, Base
-from os import getenv
+import os
 from sqlalchemy.orm import relationship
 from sqlalchemy import Column, String
 
 
 class User(BaseModel, Base):
     """Representation of a user """
-    if getenv('HBNB_TYPE_STORAGE') == 'db':
-        __tablename__ = 'users'
-        email = Column(String(128),
-                       nullable=False)
-        _password = Column('password',
-                           String(128),
-                           nullable=False)
-        first_name = Column(String(128),
-                            nullable=True)
-        last_name = Column(String(128),
-                           nullable=True)
-        places = relationship("Place",
-                              backref="user",
-                              cascade="all, delete-orphan")
-        reviews = relationship("Review",
-                               backref="user",
-                               cascade="all, delete-orphan")
+    __tablename__ = "users"
+    if os.getenv('HBNB_TYPE_STORAGE') == 'db':
+        email = Column(String(128), nullable=False)
+        password = Column(String(128), nullable=False)
+        first_name = Column(String(128), nullable=True)
+        last_name = Column(String(128), nullable=True)
+        places = relationship("Place", passive_deletes=True, backref="user")
+        reviews = relationship("Review", passive_deletes=True, backref="user")
     else:
         email = ""
-        _password = ""
+        password = ""
         first_name = ""
         last_name = ""
-
-    def __init__(self, *args, **kwargs):
-        """initializes user"""
-        super().__init__(*args, **kwargs)
-
-    @property
-    def password(self):
-        return self._password
-
-    @password.setter
-    def password(self, pwd):
-        """hashing password values"""
-        self._password = pwd
