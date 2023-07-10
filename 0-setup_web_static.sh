5f840ec1a8b3ba4fa3deedadc79a4a8c9e519110
ndandax <sebandandala99@gmail.com>
2023-07-10 16:49:14 +0300

    airbnb

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,19 +1,20 @@
 #!/usr/bin/env bash
-# Setup a web servers for the deployment of web_static.
-apt update -y
-apt install -y nginx
+#set up web servers for the deployment of web_static
+sudo apt-get -y update
+sudo apt-get -y install nginx
+sudo ufw allow 'Nginx HTTP'
+mkdir -p /data/web_static/
 mkdir -p /data/web_static/releases/test/
 mkdir -p /data/web_static/shared/
-echo "<!DOCTYPE html>
-<html>
-  <head>
-  </head>
-  <body>
-    <p>Holberton School</p>
-  </body>
-</html>" | tee /data/web_static/releases/test/index.html
-ln -sf /data/web_static/releases/test/ /data/web_static/current
+echo "<html>
+    <head>
+    </head>
+    <body>
+        Holberton School
+    </body>
+</html>" > /data/web_static/releases/test/index.html
+ln -sf /data/web_static/releases/test /data/web_static/current
 chown -R ubuntu:ubuntu /data
-sudo sed -i '39 i\ \tlocation /hbnb_static {\n\t\talias /data/web_static/current;\n\t}\n' /etc/nginx/sites-enabled/default
-sudo service nginx restart
+sed -i '/listen 80 default_server/a location /hbnb_static/ { alias /data/web_static/current/;}' /etc/nginx/sites-available/default
+service nginx restart
 exit 0
