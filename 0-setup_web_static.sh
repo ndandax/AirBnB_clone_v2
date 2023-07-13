0ec35c2231b45e336a7607745a56c0084b192a09
ndandax <sebandandala99@gmail.com>
2023-07-13 20:46:02 +0300

    deploy3

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,20 +1,32 @@
 #!/usr/bin/env bash
-#set up web servers for the deployment of web_static
+# a script to setup my web servers for the deployment of web_static
+
+# update packages and install nginx
 sudo apt-get -y update
 sudo apt-get -y install nginx
-sudo ufw allow 'Nginx HTTP'
-mkdir -p /data/web_static/
-mkdir -p /data/web_static/releases/test/
-mkdir -p /data/web_static/shared/
-echo "<html>
-    <head>
-    </head>
-    <body>
-        Holberton School
-    </body>
-</html>" > /data/web_static/releases/test/index.html
-ln -sf /data/web_static/releases/test /data/web_static/current
-chown -R ubuntu:ubuntu /data
-sed -i '/listen 80 default_server/a location /hbnb_static/ { alias /data/web_static/current/;}' /etc/nginx/sites-available/default
-service nginx restart
-exit 0
+
+# creating paths
+sudo mkdir -p /data/web_static/releases/test/
+sudo mkdir -p /data/web_static/shared
+sudo touch /data/web_static/releases/test/index.html
+
+# Print Welcome message
+echo "Welcome to my site!"  >> /data/web_static/releases/test/index.html
+
+# Check if directory current exist
+if [ -d "/data/web_static/current" ]
+then
+        sudo rm -rf /data/web_static/current
+fi
+
+# creating a symbolink link  linked to the folder
+sudo ln -s -f /data/web_static/releases/test/ /data/web_static/current
+
+# give ownership of the /data/ folder to the ubuntu user AND group
+sudo chown -hR ubuntu:ubuntu /data/
+
+# Update the Nginx configuration to serve the file content
+sudo sed -i '38i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t\tautoindex off;\n\t}\n' /etc/nginx/sites-enabled/default
+
+# restart nginx server
+sudo service nginx restart
