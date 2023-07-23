cc9431c83ae362ea613edb2c37146217ad5636a8
ndandax <sebandandala99@gmail.com>
2023-07-23 12:37:50 +0300

    web7

--- models/__init__.py
+++ models/__init__.py
@@ -1,12 +1,6 @@
 #!/usr/bin/python3
-<<<<<<< HEAD
-"""This module instantiates an object of class FileStorage"""
-import os immport getenv
-=======
-"""This module allows to change the storage type"""
+"""This module allows changing the storage type"""
 from os import getenv
->>>>>>> b7bd53f53b3d33e16d6c38c4ef24682b48d1f8c9
-
 
 if getenv("HBNB_TYPE_STORAGE") == "db":
     from models.engine.db_storage import DBStorage
